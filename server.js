194396d436f3c532d00da39587663c8d64ab8dd7
Ronnie <ronniesong0809@gmail.com>
2019-08-05 10:46:55 -0700

    feat: scraping calagator

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const cheerio = require('cheerio')
-const $ = cheerio.load('<h2 class="title">Hello world</h2>')
- 
-$('h2.title').text('Hello there!')
-$('h2').addClass('welcome')
- 
-$.html()
+var request = require('request')
+var cheerio = require('cheerio')
+
+request('http://calagator.org/events.html', function (error, response, html) {
+  console.log('error: ', error)
+  console.log('statusCode:', response && response.statusCode)
+  console.log('html:', html)
+
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html)
+
+    $('.vevent').each(function () {
+      var daysOfWeek = $(this).find('.day_of_week').text().trim().replace(/\s\s+/g, ' ')
+      var date = $(this).find('.date').clone().children().remove().end().text().trim().replace(/\s\s+/g, ' ')
+      var start = $(this).find('.dtstart').text().trim().replace(/\s\s+/g, ' ')
+      var end = $(this).find('.dtend').text().trim().replace(/\s\s+/g, ' ')
+      var summary = $(this).find('.summary').text()
+      var location = $(this).find('.location').text()
+
+      console.log('Date:\t' + date + ', ' + daysOfWeek+' from ' +start+'-'+end + '\nEvent:\t' + summary + '\nLoc:\t' + location + '\n')
+    })
+  }
+})
